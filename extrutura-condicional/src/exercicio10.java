e8a905722dc9366638ad5020667ee49d99f3b1cc
Thiago Napoli <71907294+knapoli@users.noreply.github.com>
2021-02-08 15:33:42 -0300

    Add files via upload

--- extrutura-condicional/src/exercicio10.java
+++ extrutura-condicional/src/exercicio10.java
@@ -1,8 +1,39 @@
 import java.util.Scanner;
 
 public class exercicio10 {
-	public static void main (String[]args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Entre com o numero: ");
+		int x = sc.nextInt();
+		String dia;
 		
+		switch (x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terca";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "sabado";
+			break;
+		default:
+			dia = "valor invalido";
+			break;
+		}
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
 	}
 }
